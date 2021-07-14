1c26e9ebc8c8f3601a30ed488d42244f7496ead8
ErkiKadhafi <erki.qaedhafi@gmail.com>
2021-07-14 15:02:11 +0700

    pdf report (not support)

---
+++ pertemuan21/hapus.php
@@ -0,0 +1,25 @@
+<?php 
+    //start session
+    session_start();
+    if(!isset($_SESSION["login"])){
+        header("Location: login.php");
+    }
+    
+    require "functions.php";
+
+    if(delete($_GET["id"]) > 0){
+        echo "
+            <script>
+                alert('data berhasil dihapus!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    }else{
+        echo "
+            <script>
+                alert('data gagal dihapus!');
+                document.location.href = 'index.php';
+            </script>
+        ";
+    }
+?>
